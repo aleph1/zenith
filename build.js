243f1882b3c9e97448ececc9a60637bb7a58340b
Daniel Barber <d.barber@aleph-1.com>
2023-02-23 18:20:05 -0400

    Build process now creates debug and production.

--- build.js
+++ build.js
@@ -1,7 +1,30 @@
 const rollup = require('rollup').rollup;
+const replace = require('rollup-plugin-replace');
 
+// DEBUG BUILD
 rollup({
-  input: 'src/index.js'
+  input: 'src/index.js',
+  plugins: [
+    replace({
+      DEBUG: 'true'
+    })
+  ]
+}).then( bundle => {
+  bundle.write({
+    file: 'dist/zenith.debug.js',
+    format: 'iife',
+    name: 'z'
+  })
+});
+
+// PRODUCTION BUILD
+rollup({
+  input: 'src/index.js',
+  plugins: [
+    replace({
+      DEBUG: 'false'
+    })
+  ]
 }).then( bundle => {
   bundle.write({
     file: 'dist/zenith.js',
