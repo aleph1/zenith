399c9c45b8a16c3b58a9640766e636ab9755b721
Daniel Barber <d.barber@aleph-1.com>
2023-02-23 19:48:24 -0400

    elem function accounts for passed attributes.

--- src/index.ts
+++ src/index.ts
@@ -23,7 +23,16 @@ function elem(selector: string): VnodeElem;
 function elem(selector: string, attrs?:VnodeAttributes, ...children?:Array<any>): VnodeElem;
 function elem(selector: string): VnodeElem {
   const children = [];
-  let attrs = {};
+  let start = 1;
+  let attrs = arguments[1];
+
+  // if no passed attributes
+  if(attrs == null || typeof attrs !== 'object' || attrs.tag != null || Array.isArray(attrs)) {
+    attrs = {};
+  // otherwise, copy attributes
+  } else {
+    attrs = Object.assign( {}, attrs );
+  }
   return {
     tag: selector,
     attrs,
