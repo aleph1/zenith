50c419f6063707329dc7b4f9841ea1463d92be65
Daniel Barber <d.barber@aleph-1.com>
2023-02-23 18:54:52 -0400

    Moved build script to rollup.config.js

---
+++ rollup.config.js
@@ -0,0 +1,37 @@
+import replace from '@rollup/plugin-replace';
+import typescript from '@rollup/plugin-typescript';
+
+export default [
+  // debug build
+  {
+    input: 'src/index.ts',
+    output: {
+      file: 'dist/zenith.debug.js',
+      format: 'iife',
+      name: 'z'
+    },
+    plugins: [
+      replace({
+        preventAssignment: true,
+        DEBUG: true
+      }),
+      typescript()
+    ]
+  },
+  // production build
+  {
+    input: 'src/index.ts',
+    output: {
+      file: 'dist/zenith.js',
+      format: 'iife',
+      name: 'z'
+    },
+    plugins: [
+      replace({
+        preventAssignment: true,
+        DEBUG: false
+      }),
+      typescript()
+    ]
+  }
+];
