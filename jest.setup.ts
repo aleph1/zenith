1f79dd5947eea0821a30e6505c4b75c2f4a7ae40
Daniel Barber <d.barber@aleph-1.com>
2023-02-27 13:25:35 -0400

    Added custom toHaveOnlyProperties matcher.

---
+++ jest.setup.ts
@@ -0,0 +1,37 @@
+const matchers = {
+  toHaveOnlyProperties(
+    // a matcher is a method, it has access to Jest context on `this`
+    this: jest.MatcherContext,
+    received: Object,
+    expected: Array<string>
+  ) {
+    const receivedKeys:Array<string> = Object.keys(received);
+    const expectedKeys:Array<string> = Array.from(new Set<string>(expected));
+    if(receivedKeys.length !== expectedKeys.length) return {
+      pass: false,
+      message: () => 'Received object contains ' + ( receivedKeys.length < expectedKeys.length ? 'less' : 'more' ) + ' properties than expected'
+    }
+    // validate expected keys
+    const missingKeys = expectedKeys.filter(key => !receivedKeys.includes(key));
+    if(missingKeys.length !== 0) return {
+      pass: false,
+      message: () => 'Received object missing properties: ' + missingKeys.join(', ')
+    }
+    return {
+      pass: true,
+      message: () => 'Received object has only expected properties'
+    }
+  },
+};
+
+expect.extend(matchers);
+
+declare global {
+  namespace jest {
+    interface Matchers<R> {
+      toHaveOnlyProperties(expected: Array<string>): R
+    }
+  }
+}
+
+export {};
