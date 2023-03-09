3e49f77e2e871f1941e0a0ca797776b7bef37e3d
Daniel Barber <d.barber@aleph-1.com>
2023-03-09 15:13:00 -0400

    Updated demo.

--- demo/js/main.js
+++ demo/js/main.js
@@ -55,10 +55,10 @@ const Timer = z.compDef( {
 			instance.redraw();
 		}, 1000);
 	},
-	view: instance => z.elem('article', z.elem('article',
+	view: instance => z.elem('article',
 		z.elem('h3', z.text('Timer Demo')),
 		z.elem('div', z.text('Timer: ' + instance.state.counter))
-	))
+	)
 } );
 
 const List = z.compDef( {
@@ -67,7 +67,7 @@ const List = z.compDef( {
 		instance.state.counter = 0;
 		instance.state.items = [...Array(10).fill(0).map(() => instance.state.counter++)];
 	},
-	view: instance => z.elem('article', z.elem('article',
+	view: instance => z.elem('article',
 		z.elem('h3', z.text('List Demo')),
 		z.elem('div',
 			z.elem('button', {
@@ -86,37 +86,68 @@ const List = z.compDef( {
 							instance.state.items.splice(instance.state.items.indexOf(item), 1);
 							instance.redraw();
 						}
-					}, z.text('remove'))
+					}, z.text('x'))
 				)
 			)
-		)
-	))
-} );
-
-const Header = z.compDef( {
-	view: instance => z.elem('header', z.text('Zenith.js'))
-} );
-
-const Main = z.compDef( {
-	view: instance => z.elem('main',
-		z.elem('section',
-			z.comp(Counter),
-			z.comp(Timer),
-			z.comp(List),
 		)
 	)
 } );
 
+const Header = z.compDef( {
+	view: instance => z.elem( 'header',
+		{
+			id: 'header'
+		},
+		z.elem('div',
+			z.elem('div', {
+				class: 'logo'
+			}),
+			z.elem('div', {
+				class: 'info'
+			},
+				z.elem('h1', z.text('Zenith.js is a tiny yet powerful GUI framework that is under 2kb minified and gzipped.')),
+				z.elem('p', z.text('Its vdom and diffing algorithm function on an entire page or on specific nodes — especially useful in cases where you know that only a portion of the tree will change. Zenith.js is fast, flexible, and doesn’t require a compile step.')),
+				z.elem('p', z.text('This site uses Zenith.js.')),
+			)
+		)
+	)
+} );
+
+const Main = z.compDef( {
+	view: instance => z.elem('main',
+		z.elem('div',
+			z.comp(instance.attrs.comp)
+		)
+	)
+} );
+
+const Demos = z.compDef( {
+	view: instance => z.elem('section',
+		z.comp(Counter),
+		z.comp(Timer),
+		z.comp(List),
+	)
+} );
+
 const Footer = z.compDef( {
-	view: instance => z.elem('footer', z.text('Copyright ©2022 Aleph1 / Daniel Barber'))
+	view: instance => z.elem('footer', {
+		id: 'footer'
+	}, z.elem('div',
+		z.text('Copyright ©2022 Aleph1 / Daniel Barber'))
+	)
 } );
 
 const Layout = z.compDef( {
 	view: instance => [
 		z.comp(Header),
-		z.comp(Main),
+		z.comp(Main, {
+			comp: instance.attrs.comp
+		}),
 		z.comp(Footer),
 	]
 } );
 
-z.draw(document.querySelector('#app'), z.comp(Layout));
+z.draw(document.querySelector('#app'), z.comp(Layout, {
+	comp: Demos
+}));
+setTimeout(() => document.querySelector('html').classList.add('loaded'), 1);
