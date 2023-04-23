f1e622fa9c054f8ebf54e2de955ea61689667488
Daniel Barber <d.barber@aleph-1.com>
2023-04-23 12:40:15 -0400

    Added requestAnimationFrame support

--- jest.setup.ts
+++ jest.setup.ts
@@ -1,3 +1,21 @@
+global.BROWSER = false;
+global.FRAME_TIME = 16;
+
+Object.defineProperty(window, 'navigator', {value: 'node'});
+Object.defineProperty(window, 'userAgent', {value: 'node'});
+Object.defineProperty(window, 'requestAnimationFrame', {value: (callback:any) =>{}});
+
+jest.useFakeTimers();
+const spiedRequestAnimationFrame = jest.spyOn(window, 'requestAnimationFrame');
+spiedRequestAnimationFrame.mockImplementation((callback: FrameRequestCallback): number => {
+  return window.setTimeout(callback, global.FRAME_TIME);
+});
+
+afterAll(() => {
+  spiedRequestAnimationFrame.mockRestore();
+  jest.clearAllTimers();
+});
+
 const matchers = {
   toHaveOnlyProperties(
     // a matcher is a method, it has access to Jest context on `this`
