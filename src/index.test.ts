61352ef754ba979040c51806db5bccbbed04c2c2
Daniel Barber <d.barber@aleph-1.com>
2023-04-23 12:41:29 -0400

    Added tests for sorting

--- src/index.test.ts
+++ src/index.test.ts
@@ -722,6 +722,92 @@ describe('DOM', () => {
 
   });
 
+  describe('Sorting', () => {
+
+    test('unkeyed nodes sort as expected when redrawn deferred', () => {
+      document.body.innerHTML = '<div id="app"></div>';
+      const app = document.querySelector('#app');
+      const ids = [0,1,2];
+      const UnkeyedList = z.compDef({
+        draw: vNode => z.elem('ul', ids.map(id => z.elem('li', z.text(id))))
+      })
+      const list = z.comp(UnkeyedList);
+      z.draw(app, list);
+      expect(list.children[0].children[0].children[0].tag).toEqual('0');
+      expect(list.children[0].children[1].children[0].tag).toEqual('1');
+      expect(list.children[0].children[2].children[0].tag).toEqual('2');
+      [ids[0], ids[2]] = [ids[2], ids[0]];
+      list.redraw();
+      jest.advanceTimersByTime(global.FRAME_TIME);
+      expect(list.children[0].children[0].children[0].tag).toEqual('2');
+      expect(list.children[0].children[1].children[0].tag).toEqual('1');
+      expect(list.children[0].children[2].children[0].tag).toEqual('0');
+    });
+
+    test('unkeyed nodes sort as expected when redrawn immediately', () => {
+      document.body.innerHTML = '<div id="app"></div>';
+      const app = document.querySelector('#app');
+      const ids = [0,1,2];
+      const UnkeyedList = z.compDef({
+        draw: vNode => z.elem('ul', ids.map(id => z.elem('li', z.text(id))))
+      })
+      const list = z.comp(UnkeyedList);
+      z.draw(app, list);
+      expect(list.children[0].children[0].children[0].tag).toEqual('0');
+      expect(list.children[0].children[1].children[0].tag).toEqual('1');
+      expect(list.children[0].children[2].children[0].tag).toEqual('2');
+      [ids[0], ids[2]] = [ids[2], ids[0]];
+      list.redraw(true);
+      expect(list.children[0].children[0].children[0].tag).toEqual('2');
+      expect(list.children[0].children[1].children[0].tag).toEqual('1');
+      expect(list.children[0].children[2].children[0].tag).toEqual('0');
+    });
+
+    test('keyed nodes sort as expected when redrawn deferred', () => {
+      document.body.innerHTML = '<div id="app"></div>';
+      const app = document.querySelector('#app');
+      const ids = [0,1,2];
+      const UnkeyedList = z.compDef({
+        draw: vNode => z.elem('ul', ids.map(id => z.elem('li', {
+          key: id
+        }, z.text(id))))
+      })
+      const list = z.comp(UnkeyedList);
+      z.draw(app, list);
+      expect(list.children[0].children[0].children[0].tag).toEqual('0');
+      expect(list.children[0].children[1].children[0].tag).toEqual('1');
+      expect(list.children[0].children[2].children[0].tag).toEqual('2');
+      [ids[0], ids[2]] = [ids[2], ids[0]];
+      list.redraw();
+      jest.advanceTimersByTime(global.FRAME_TIME);
+      expect(list.children[0].children[0].children[0].tag).toEqual('2');
+      expect(list.children[0].children[1].children[0].tag).toEqual('1');
+      expect(list.children[0].children[2].children[0].tag).toEqual('0');
+    });
+
+    test('keyed nodes sort as expected when redrawn immediately', () => {
+      document.body.innerHTML = '<div id="app"></div>';
+      const app = document.querySelector('#app');
+      const ids = [0,1,2];
+      const UnkeyedList = z.compDef({
+        draw: vNode => z.elem('ul', ids.map(id => z.elem('li', {
+          key: id
+        }, z.text(id))))
+      })
+      const list = z.comp(UnkeyedList);
+      z.draw(app, list);
+      expect(list.children[0].children[0].children[0].tag).toEqual('0');
+      expect(list.children[0].children[1].children[0].tag).toEqual('1');
+      expect(list.children[0].children[2].children[0].tag).toEqual('2');
+      [ids[0], ids[2]] = [ids[2], ids[0]];
+      list.redraw(true);
+      expect(list.children[0].children[0].children[0].tag).toEqual('2');
+      expect(list.children[0].children[1].children[0].tag).toEqual('1');
+      expect(list.children[0].children[2].children[0].tag).toEqual('0');
+    });
+
+  });
+
   describe('Events with z.elem()', () => {
 
     test('onclick event is called', () => {
