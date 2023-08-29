840c0597975c1c73a359d00386ac50b17ce03b1f
Daniel Barber <d.barber@aleph-1.com>
2023-08-29 11:24:41 -0400

    Refactored Handles null

--- src/index.test.ts
+++ src/index.test.ts
@@ -424,11 +424,13 @@ describe('vNode', () => {
     });
 
     test('Handles null', () => {
-      expect(vNode).not.toBeNull();
-      expect(typeof vNode).toBe('object');
-      expect(vNode).toHaveOnlyProperties(['type', 'tag']);
-      expect(vNode.type).toBe(VNodeTypes.text);
-      expect(vNode.tag).toBe('');
+      // eslint-disable-next-line
+      const node = z.text(null as any); // eslint-disable-line @typescript-eslint/no-explicit-any
+      expect(node).not.toBeNull();
+      expect(typeof node).toBe('object');
+      expect(node).toHaveOnlyProperties(['type', 'tag']);
+      expect(node.type).toBe(VNodeTypes.text);
+      expect(node.tag).toBe('');
     });
 
     test('Handles undefined', () => {
