456eb0668efce4c503e9eacb0ed4d2a211379cf2
Daniel Barber <d.barber@aleph-1.com>
2023-08-29 12:18:01 -0400

    Refactored test Keyed z.elem() sort as expected when keys removed

--- src/index.test.ts
+++ src/index.test.ts
@@ -2452,24 +2452,27 @@ describe('DOM', () => {
       expect(list.children[0].children[1].children[0].tag).toEqual('0');
     });
 
-    test('keyed nodes sort as expected when redrawn deferred', () => {
+    test('Keyed z.elem() sort as expected when keys removed', () => {
       document.body.innerHTML = '<div id="app"></div>';
       const app = document.querySelector('#app');
-      const ids = [0,1,2];
+      const ids = [0, 1, 2];
       const UnkeyedList = z.compDef({
         draw: vNode => z.elem('ul', ids.map(id => z.elem('li', {
           key: id
         }, z.text(id))))
       })
       const list = z.comp(UnkeyedList);
-      z.draw(app, list);
+      z.mount(app, list);
       expect(list.children[0].children[0].children[0].tag).toEqual('0');
       expect(list.children[0].children[1].children[0].tag).toEqual('1');
       expect(list.children[0].children[2].children[0].tag).toEqual('2');
-      [ids[0], ids[2]] = [ids[2], ids[0]];
+      ids.splice(1, 1);
       list.redraw();
       jest.advanceTimersByTime(global.FRAME_TIME);
-      expect(list.children[0].children[0].children[0].tag).toEqual('2');
+      expect(list.children[0].children[0].children[0].tag).toEqual('0');
+      expect(list.children[0].children[1].children[0].tag).toEqual('2');
+    });
+
       expect(list.children[0].children[1].children[0].tag).toEqual('1');
       expect(list.children[0].children[2].children[0].tag).toEqual('0');
     });
