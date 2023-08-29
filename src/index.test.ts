0309fe0e45e1bc7d1c879e7169b78bc132abb70e
Daniel Barber <d.barber@aleph-1.com>
2023-08-29 12:03:01 -0400

    Added test mounted child changes from z.text() to z.comp()

--- src/index.test.ts
+++ src/index.test.ts
@@ -1618,6 +1618,29 @@ describe('DOM', () => {
       expect(mountedNode.children[0].dom).toEqual(elem2);
     });
 
+    test('mounted child changes from z.text() to z.comp()', () => {
+      document.body.innerHTML = '<div id="app"></div>';
+      const app = document.querySelector('#app');
+      const compDef = z.compDef({
+        draw: vNode => z.elem('div', z.text(vNode.attrs.text))
+      });
+      const node1 = z.text('test2');
+      const node2 = z.comp(compDef, {
+        text: 'test1'
+      });
+      const elem1 = document.createTextNode('test2');
+      const elem2 = document.createElement('div');
+      elem2.innerHTML = 'test1';
+      const mountedNode = z.mount(app, node1);
+      expect(mountedNode.children.length).toBe(1);
+      expect(mountedNode.children[0].dom).toEqual(elem1);
+      z.mount(app, node2);
+      expect(mountedNode.children.length).toBe(1);
+      expect(mountedNode.children[0]).toEqual(node2);
+      expect(mountedNode.children[0].tag).toEqual(compDef);
+      expect(mountedNode.children[0].children[0].dom).toEqual(elem2);
+    });
+
 
   });
     });
