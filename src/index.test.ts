3ae5a469af456aae67fea11c90be16475a814ecd
Daniel Barber <d.barber@aleph-1.com>
2023-08-29 12:00:31 -0400

    Added test array of z.text() mounted, unmounted, and remounted with different order

--- src/index.test.ts
+++ src/index.test.ts
@@ -1246,6 +1246,30 @@ describe('DOM', () => {
       expect(mountedNode.children[1].dom).toEqual(elem3);
     });
 
+    test('array of z.text() mounted, unmounted, and remounted with different order', () => {
+      document.body.innerHTML = '<div id="app"></div>';
+      const app = document.querySelector('#app');
+      const node1 = z.text('test1');
+      const node2 = z.text('test2');
+      const elem1 = document.createTextNode('test1');
+      const elem2 = document.createTextNode('test2');
+      let mountedNode = z.mount(app, [node1, node2]);
+      expect(mountedNode.children.length).toEqual(2);
+      expect(mountedNode.children[0]).toBe(node1);
+      expect(mountedNode.children[0].dom).toEqual(elem1);
+      expect(mountedNode.children[1]).toBe(node2);
+      expect(mountedNode.children[1].dom).toEqual(elem2);
+      mountedNode = z.mount(app, null);
+      expect(mountedNode.children.length).toEqual(0);
+      expect(node1.dom).toEqual(undefined);
+      expect(node2.dom).toEqual(undefined);
+      mountedNode = z.mount(app, [node2, node1]);
+      expect(mountedNode.children[0]).toBe(node2);
+      expect(mountedNode.children[0].dom).toEqual(elem2);
+      expect(mountedNode.children[1]).toBe(node1);
+      expect(mountedNode.children[1].dom).toEqual(elem1);
+    });
+
 
   });
     });
