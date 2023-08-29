7bcdad24f4430743d33c7cb0e31cce39bfb6f7fc
Daniel Barber <d.barber@aleph-1.com>
2023-08-29 12:12:09 -0400

    Refactored test z.comp() child element namespace is correct when node changes from xhtml to svg

--- src/index.test.ts
+++ src/index.test.ts
@@ -2092,7 +2092,24 @@ describe('DOM', () => {
       expect((node.doms[0] as Element).namespaceURI).toEqual('http://www.w3.org/1999/xhtml');
     });
 
-    test('Attribute of null is equivalent to no attribute', () => {
+    test('z.comp() child element namespace is correct when node changes from xhtml to svg', () => {
+      const app = document.querySelector('#app');
+      let elType = 'div';
+      const compDef = z.compDef({
+        draw: vNode => z.elem(elType)
+      });
+      const node = z.comp(compDef);
+      z.mount(app, node);
+      expect(node.children.length).toBe(1);
+      expect(node.doms[0].nodeName.toLowerCase()).toBe('div');
+      expect((node.doms[0] as Element).namespaceURI).toEqual('http://www.w3.org/1999/xhtml');
+      elType = 'svg';
+      node.redraw(true);
+      expect(node.children.length).toBe(1);
+      expect(node.doms[0].nodeName.toLowerCase()).toBe('svg');
+      expect((node.doms[0] as Element).namespaceURI).toEqual('http://www.w3.org/2000/svg');
+    });
+
       document.body.innerHTML = '<div id="app"></div>';
       const app = document.querySelector('#app');
       const el1 = z.elem('div', {id: null});
