3313bd43253f92b2f91a27062a1f0c3ba85f1237
Daniel Barber <d.barber@aleph-1.com>
2023-08-29 11:12:52 -0400

    Removed isKeyed check

--- src/index.ts
+++ src/index.ts
@@ -85,7 +85,6 @@ const normalizeChildren = (vNode:VNodeContainer, children:VNodeArray): VNodeFlat
   const normalizedChildren:VNodeFlatArray = children.flat(Infinity) as VNodeFlatArray;
   const firstChild = normalizedChildren[0];
   const isKeyed = !firstChild || typeof firstChild === 'boolean' || (firstChild.type !== VNodeTypes.elem && firstChild.type !== VNodeTypes.comp) || !Object.prototype.hasOwnProperty.call(firstChild.attrs, 'key') ? false : true;
-  if (isKeyed) vNode.keys = true;
   for(const [index, child] of normalizedChildren.entries()) {
     // convert all falsy children to null
     if (!child || typeof child === 'boolean') {
