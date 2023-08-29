190a18c3cb33371ce909b6cff3573ae1a4904dce
Daniel Barber <d.barber@aleph-1.com>
2023-08-29 12:23:39 -0400

    Added test z.comp() lifecycle hooks are called in correct order

--- src/index.test.ts
+++ src/index.test.ts
@@ -3045,6 +3045,61 @@ describe('DOM', () => {
       expect(destroyFn).toHaveBeenCalledTimes(1);
     });
 
+    test('z.comp() lifecycle hooks are called in correct order', () => {
+      document.body.innerHTML = '<div id="app"></div>';
+      const app = document.querySelector('#app');
+      const initFn = jest.fn();
+      const drawFn = jest.fn(() => z.elem('div'));
+      const drawnFn = jest.fn();
+      const removeFn = jest.fn();
+      const destroyFn = jest.fn();
+      const compDef = z.compDef({
+        init: initFn,
+        draw: drawFn,
+        drawn: drawnFn,
+        remove: removeFn,
+        destroy: destroyFn,
+      });
+      const initSpy = jest.spyOn(compDef, 'init');
+      const drawSpy = jest.spyOn(compDef, 'draw');
+      const drawnSpy = jest.spyOn(compDef, 'drawn');
+      const removeSpy = jest.spyOn(compDef, 'remove');
+      const destroySpy = jest.spyOn(compDef, 'destroy');
+      const vNode = z.comp(compDef);
+      expect(initFn).toHaveBeenCalledTimes(0);
+      expect(drawFn).toHaveBeenCalledTimes(0);
+      expect(drawFn).toHaveBeenCalledTimes(0);
+      expect(removeFn).toHaveBeenCalledTimes(0);
+      expect(destroyFn).toHaveBeenCalledTimes(0);
+      z.mount(app, vNode);
+      expect(initFn).toHaveBeenCalledTimes(1);
+      expect(drawFn).toHaveBeenCalledTimes(1);
+      expect(drawFn).toHaveBeenCalledTimes(1);
+      expect(removeFn).toHaveBeenCalledTimes(0);
+      expect(destroyFn).toHaveBeenCalledTimes(0);
+      vNode.redraw(true);
+      expect(initFn).toHaveBeenCalledTimes(1);
+      expect(drawFn).toHaveBeenCalledTimes(2);
+      expect(drawFn).toHaveBeenCalledTimes(2);
+      expect(removeFn).toHaveBeenCalledTimes(0);
+      expect(destroyFn).toHaveBeenCalledTimes(0);
+      z.mount(app, null);
+      expect(initFn).toHaveBeenCalledTimes(1);
+      expect(drawFn).toHaveBeenCalledTimes(2);
+      expect(drawFn).toHaveBeenCalledTimes(2);
+      expect(removeFn).toHaveBeenCalledTimes(1);
+      expect(destroyFn).toHaveBeenCalledTimes(1);
+      const initOrder = initSpy.mock.invocationCallOrder[0];
+      const drawOrder = drawSpy.mock.invocationCallOrder[0];
+      const drawnOrder = drawnSpy.mock.invocationCallOrder[0];
+      const removeOrder = removeSpy.mock.invocationCallOrder[0];
+      const destroyOrder = destroySpy.mock.invocationCallOrder[0];
+      expect(initOrder).toBeLessThan(drawOrder);
+      expect(drawOrder).toBeLessThan(drawnOrder);
+      expect(drawnOrder).toBeLessThan(removeOrder);
+      expect(removeOrder).toBeLessThan(destroyOrder);
+    });
+
 
     test('input value is updated when DOM value differs from vNode value', () => {
       document.body.innerHTML = '<div id="app"></div>';
