280aeb8fd73943f4992ceaec9baf761ceaa42dcc
Daniel Barber <d.barber@aleph-1.com>
2023-08-29 12:03:17 -0400

    Added test mounted child changes from z.text() to z.elem()

--- src/index.test.ts
+++ src/index.test.ts
@@ -1659,6 +1659,24 @@ describe('DOM', () => {
       expect(mountedNode.children[0].dom).toEqual(elem2);
     });
 
+    test('mounted child changes from z.text() to z.elem()', () => {
+      document.body.innerHTML = '<div id="app"></div>';
+      const app = document.querySelector('#app');
+      const node1 = z.text('test1');
+      const node2 = z.elem('div', z.text('test2'));
+      const elem1 = document.createTextNode('test1');
+      const elem2 = document.createElement('div');
+      elem2.innerHTML = 'test2';
+      const mountedNode = z.mount(app, node1);
+      expect(mountedNode.children.length).toBe(1);
+      expect(mountedNode.children[0]).toEqual(node1);
+      expect(mountedNode.children[0].dom).toEqual(elem1);
+      z.mount(app, node2);
+      expect(mountedNode.children.length).toBe(1);
+      expect(mountedNode.children[0]).toEqual(node2);
+      expect(mountedNode.children[0].dom).toEqual(elem2);
+    });
+
 
   });
     });
