13b29bb0694631dd25a370f3060485dda1408c2a
Daniel Barber <d.barber@aleph-1.com>
2023-08-29 11:55:41 -0400

    Refactored test z.html() with <caption>

--- src/index.test.ts
+++ src/index.test.ts
@@ -938,10 +938,10 @@ describe('DOM', () => {
     test('z.html() with <caption>', () => {
       document.body.innerHTML = '<table></table>';
       const app = document.querySelector('table');
-      const vNode = z.html('<caption>');
+      const node = z.html('<caption>');
       const elem1 = document.createElement('caption');
-      z.draw(app, vNode);
-      expect(vNode.dom[0]).toEqual(elem1);
+      z.mount(app, node);
+      expect(node.doms[0]).toEqual(elem1);
     });
 
     test('z.html() with <col>', () => {
